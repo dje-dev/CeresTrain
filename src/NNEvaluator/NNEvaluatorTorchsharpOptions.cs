3974a8073bbebb6a792bd8145066a0f47a039c2f
dje <elliodj@verizon.net>
2024-02-04 21:33:28 -0500

    Cleanup class NNEvaluatorTorchsharpOptions.

--- src/NNEvaluator/NNEvaluatorTorchsharpOptions.cs
+++ src/NNEvaluator/NNEvaluatorTorchsharpOptions.cs
@@ -11,15 +11,49 @@ namespace CeresTrain.NNEvaluators
   /// </summary>
   public record class NNEvaluatorTorchsharpOptions
   {
+    /// <summary>
+    /// Assumed magnitude (Q units) of adverse blunders that will follow in the game.
+    /// </summary>
     public readonly float QNegativeBlunders;
+
+    /// <summary>
+    /// Assumed magnitude (Q units) of favorable blunders that will follow in the game.
+    /// </summary>
     public readonly float QPositiveBlunders;
+
+    /// <summary>
+    /// Fraction of the value head 2 that is used to blend into the primary value.
+    /// </summary>
     public readonly float FractionValueHead2;
+
+    /// <summary>
+    /// If true, monitor activations of the neural network. 
+    /// </summary>
     public readonly bool MonitorActivations;
+
+    /// <summary>
+    /// Temperature for the value head 1.
+    /// </summary>
     public readonly float ValueHead1Temperature;
+
+    /// <summary>
+    /// Temperature for the value head 2.
+    /// </summary>
     public readonly float ValueHead2Temperature;
 
-    public NNEvaluatorTorchsharpOptions(float qNegativeBlunders, float qPositiveBlunders, 
-                                        float fractionUndeblunderedValueHead,
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="qNegativeBlunders"></param>
+    /// <param name="qPositiveBlunders"></param>
+    /// <param name="fractionUndeblunderedValueHead"></param>
+    /// <param name="monitorActivations"></param>
+    /// <param name="valueHead1Temperature"></param>
+    /// <param name="valueHead2Temperature"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public NNEvaluatorTorchsharpOptions(float qNegativeBlunders = 0, float qPositiveBlunders = 0, 
+                                        float fractionUndeblunderedValueHead = 0,
                                         bool monitorActivations = false,
                                         float valueHead1Temperature = 1,
                                         float valueHead2Temperature = 1)
