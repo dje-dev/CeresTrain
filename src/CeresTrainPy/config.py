1e83fa4842ac6db0cfefebd48bfb6c91be734104
dje <elliodj@verizon.net>
2024-06-17 21:28:15 -0400

    Default compile mode set to max-autotune in config.py.

--- src/CeresTrainPy/config.py
+++ src/CeresTrainPy/config.py
@@ -80,7 +80,7 @@ class Configuration:
     self.Opt_Optimizer = config_opt.get('Optimizer', 'AdamW')
     self.Opt_StartingCheckpointFN = config_opt.get('StartingCheckpointFN')
     self.Opt_StartingCheckpointLastPosNum = config_opt.get('StartingCheckpointLastPosNum')
-    self.Opt_PyTorchCompileMode = config_opt.get('PyTorchCompileMode', None)
+    self.Opt_PyTorchCompileMode = config_opt.get('PyTorchCompileMode', "max-autotune")
     self.Opt_WeightDecay = config_opt.get('WeightDecay', 0.01)
     self.Opt_LearningRateBase = config_opt.get('LearningRateBase', 0.0005)
     self.Opt_LRBeginDecayAtFractionComplete = config_opt.get('LRBeginDecayAtFractionComplete', 0.25)
